0d90ac80a242a88be8e2c4aad2f3f201d585734c
martinfyic <martin.f.yic@gmail.com>
2024-06-23 19:04:55 -0300

    feat: :sparkles: add payment module

---
+++ src/main.ts
@@ -0,0 +1,25 @@
+import { NestFactory } from '@nestjs/core';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { AppModule } from './app.module';
+import { envs } from './config';
+
+async function bootstrap() {
+  const logger = new Logger('Payment Microservice');
+
+  const app = await NestFactory.create(AppModule, {
+    rawBody: true,
+  });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  app.setGlobalPrefix('api');
+
+  await app.listen(envs.port);
+  logger.log(`Payment Microservice running on PORT ${envs.port}`);
+}
+bootstrap();
